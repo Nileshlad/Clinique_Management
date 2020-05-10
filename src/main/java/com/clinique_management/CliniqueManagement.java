91d929472a28413ffa14e55d677a28d20fe91c4e
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-05-10 13:49:30 +0530

    Added: Welcome Clinique Management

---
+++ src/main/java/com/clinique_management/CliniqueManagement.java
@@ -0,0 +1,7 @@
+package com.clinique_management;
+
+public class CliniqueManagement {
+    public static void main(String[] args) {
+        System.out.println("welcome to Clinique Management");
+    }
+}
