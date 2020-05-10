564486d1bbc5b398b2898cfaacbb7c72e8bf66aa
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-05-10 14:01:47 +0530

    Added:To clinique management services

---
+++ src/main/java/com/service/ICliniqueManagementService.java
@@ -0,0 +1,52 @@
+package com.service;
+
+public interface ICliniqueManagementService {
+
+    public <E> void addInformation(E data, String filePath);
+    /**
+     * This method is used to add data
+     * into json file.
+     */
+
+    public boolean searchDoctorByName(String doctorName, String filePath);
+    /**
+     * This method is used to search doctor
+     * by name from json file.
+     */
+
+    public boolean searchDoctorById(int doctorId, String doctorfilePath);
+    /**
+     * This method is used to search doctor
+     * by id from json file.
+     */
+
+    public boolean searchDoctorBySpecialization(String doctorSpecialist, String doctorfilePath);
+    /**
+     * This method is used to search doctor
+     * by specialist from json file.
+     */
+
+    public boolean searchDoctorByAvailability(String doctorAvailability, String doctorfilePath);
+    /**
+     * This method is used to search doctor
+     * by availability from json file.
+     */
+
+    public boolean searchPatientByName(String patientName, String patientFilePath);
+    /**
+     * This method is used to search patient
+     * by name from json file.
+     */
+
+    public boolean searchPatientByMobileNumber(long mobileNumber, String patientfilePath);
+    /**
+     * This method is used to search patient
+     * by mobile number from json file.
+     */
+
+    boolean searchPatientById(int id, String patientfilePath);
+    /**
+     * This method is used to search patient
+     * by id from json file.
+     */
+}
