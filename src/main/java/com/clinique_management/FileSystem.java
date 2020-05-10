3023e201c7a1bb88f51ec5c9c8ffe8ea72d9d23a
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-05-10 14:05:25 +0530

    Added: To clinique management file read and write objectmapper

---
+++ src/main/java/com/clinique_management/FileSystem.java
@@ -0,0 +1,35 @@
+package com.clinique_management;
+
+
+import com.model.Doctor;
+import com.model.Patient;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileSystem {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public <E> ArrayList<E> readFile(String filePath) throws IOException {
+        return objectMapper.readValue(new File(filePath), new TypeReference<ArrayList<E>>() {
+        });
+    }
+
+    public <E> void writeFile(ArrayList<E> list, String filePath) throws IOException {
+        objectMapper.writeValue(new File(filePath), list);
+    }
+
+    public  ArrayList<Doctor> readDoctorFile(String filePath) throws IOException {
+        return objectMapper.readValue(new File(filePath), new TypeReference<ArrayList<Doctor>>() {
+        });
+    }
+
+    public  ArrayList<Patient> readPatientFile(String filePath) throws IOException {
+        return objectMapper.readValue(new File(filePath), new TypeReference<ArrayList<Patient>>() {
+        });
+    }
+}
